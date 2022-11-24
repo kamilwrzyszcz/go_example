06411096552ce540b35f0ea489c816aeb5cc1b21
kamilwrzyszcz <kamilwrzyszcz95@gmail.com>
2022-11-24 10:46:12 +0100

    initial server startup implementation

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"log"
+
+	"github.com/kamilwrzyszcz/go_example/api"
+	db "github.com/kamilwrzyszcz/go_example/db/sqlc"
+	"github.com/kamilwrzyszcz/go_example/session"
+	"github.com/kamilwrzyszcz/go_example/util"
+)
 
 func main() {
-	fmt.Println("Hello")
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config: ", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("cannot connect to db: ", err)
+	}
+
+	store := db.NewStore(conn)
+	sessionClient, err := session.NewRedisClient(config.RedisAddress, config.RedisPassword)
+	if err != nil {
+		log.Fatal("cannot connect to session client: ", err)
+	}
+
+	server, err := api.NewServer(config, store, sessionClient)
+	if err != nil {
+		log.Fatal("cannot create server: ", err)
+	}
+
+	err = server.Start(config.ServerAddress)
+	if err != nil {
+		log.Fatal("cannot start server: ", err)
+	}
 }
